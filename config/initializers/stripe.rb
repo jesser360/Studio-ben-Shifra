3dc18c359b52e93e83e7c4db12a75cd31eef6ae0
Jesser360 <jesser360@gmail.com>
2017-05-19 20:20:12 -0700

    removed files from github and put em in ignore

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,4 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['pk_live_BcXpGXVecQ0CIgsB3vFHOlW4']
 }
 
-Stripe.api_key = "sk_live_ktGnDjDyaR9azyzmMN8VIF2N"
+Stripe.api_key = "sk_live_n0VRi83qBOFvAxGVBK8M6mec"
