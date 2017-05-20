2d84337012c90a8cb039026525717a894036e146
Jesser360 <jesser360@gmail.com>
2017-05-19 19:26:35 -0700

    did it

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,3 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_live_BcXpGXVecQ0CIgsB3vFHOlW4'],
-  :secret_key      => ENV['sk_live_on922KxNVbGVd8yCoxDIPEPw']
+  :publishable_key => ENV['pk_live_BcXpGXVecQ0CIgsB3vFHOlW4']
 }
-
-Stripe.api_key = "sk_live_on922KxNVbGVd8yCoxDIPEPw"
