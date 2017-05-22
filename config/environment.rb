7aab499a7f5e9aaaab90ecd52de926397cd60bf9
jesser360 <jesser360@gmail.com>
2017-05-21 18:44:53 -0700

    set ENV var

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require_relative 'application'
 Rails.configuration.stripe = {
   :publishable_key => ENV['pk_live_BcXpGXVecQ0CIgsB3vFHOlW4']
 }
-Stripe.api_key = "pk_live_BcXpGXVecQ0CIgsB3vFHOlW4"
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
 # Initialize the Rails application.
 Rails.application.initialize!
