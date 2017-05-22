554aa036b27ead1de57ced4804aef10eadc992d9
jesser360 <jesser360@gmail.com>
2017-05-21 18:39:24 -0700

    stuf

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ require_relative 'application'
 Rails.configuration.stripe = {
   :publishable_key => ENV['pk_live_BcXpGXVecQ0CIgsB3vFHOlW4']
 }
+Stripe.api_key = "pk_live_BcXpGXVecQ0CIgsB3vFHOlW4"
 # Initialize the Rails application.
 Rails.application.initialize!
